468507e61a861589ea1df3c3ab18eee8694c737d
Amit Kumar <toamitkumar@gmail.com>
2012-01-26 13:59:25 -0500

    Initial vanilla jruby app for powerpoint export

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerpointExport::Application.config.secret_token = '28979bc4eb2f7ccbc5d75f6580090e3768b39a17df6164049654ca533df73c7b22e2c4395fae36875f6817e760d466bf7ba8e71693381a45ca0a244b2126e825'
